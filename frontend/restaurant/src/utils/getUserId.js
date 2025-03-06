fbb805d80e8c960fbaa91477b942f5e955f5a2f7
electrikbox <electrikbox@icloud.com>
2025-03-06 13:45:58 +0100

    check if admin or user

--- frontend/restaurant/src/utils/getUserId.js
+++ frontend/restaurant/src/utils/getUserId.js
@@ -1,7 +1,10 @@
-export const getUserId = () => {
+export const getUserId = (event = null) => {
 	const storedUser = localStorage.getItem("user");
 	if (!storedUser) throw new Error("User not logged in");
 
 	const user = JSON.parse(storedUser);
+
+	if (user.role === "admin" && event) return event.event.user._id;
+
 	return user.id;
 }
