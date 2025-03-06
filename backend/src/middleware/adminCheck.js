e4ebcf23e8002d59bbe6e97b05499134044f2980
electrikbox <electrikbox@icloud.com>
2025-03-06 16:04:17 +0100

    add comment

--- backend/src/middleware/adminCheck.js
+++ backend/src/middleware/adminCheck.js
@@ -3,19 +3,25 @@ const jwt = require('jsonwebtoken');
 
 
 const adminCheck = async (req, res, next) => {
+
+    // Check if token is present in headers or cookies
     let token = req.headers.authorization;
+
     if (!token && req.cookies && req.cookies.token)
         token = req.cookies.token;
-    
+
+    // If token is not present, return error
     if (!token) {
         res.status(401).json({ error: 'Token not found' });
         return;
     }
   
     try {
+        // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
 
+        // Check if user is admin
         if (req.user.role !== 'admin') {
             res.status(403).json('Only admin can access this route');
             return;
--- backend/src/middleware/jwtAuth.js
+++ backend/src/middleware/jwtAuth.js
@@ -3,10 +3,13 @@ const jwt = require('jsonwebtoken');
 
 
 const jwtAuth = async (req, res, next) => {
+
+    // Get token from header
     let token = req.headers.authorization;
     if (!token && req.cookies && req.cookies.token)
         token = req.cookies.token;
     
+    // Check if not token
     if (!token) {
         res.status(401).json({ error: 'Token not found' });
         return;
@@ -15,6 +18,7 @@ const jwtAuth = async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded;
+
       next();
     } catch (err) {
         console.error(err);
