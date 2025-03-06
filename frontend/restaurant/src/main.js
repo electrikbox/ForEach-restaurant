5a4976595408fb1463370472ff713080511293ad
electrikbox <electrikbox@icloud.com>
2025-03-06 13:46:02 +0100

    cleaning

--- frontend/restaurant/src/main.js
+++ frontend/restaurant/src/main.js
@@ -3,11 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import Toast from "vue-toastification";
-import 'vue-toastification/dist/index.css';
-
 import App from './App.vue'
 import router from './router'
+import Toast from "vue-toastification";
+import 'vue-toastification/dist/index.css';
 
 // Importation de FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -18,7 +17,7 @@ library.add(fas);
 
 const app = createApp(App)
 
-// Ajoute toutes les icônes solides dans la bibliothèque (ou seulement celles dont tu as besoin)
+// Ajoute toutes les icônes de FontAwesome à l'application
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia())
