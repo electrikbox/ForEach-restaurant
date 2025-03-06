ad318890293ddd1e935cbc719b82d2080229c5fd
electrikbox <electrikbox@icloud.com>
2025-03-06 13:44:03 +0100

    adminCheck added

--- backend/src/routes/reservationsRoutes.js
+++ backend/src/routes/reservationsRoutes.js
@@ -1,11 +1,12 @@
 const reservationsRouter = require('express').Router();
 const reservationsController = require('../controllers/reservationsController');
-// const adminCheck = require('../middleware/adminCheck');
+const adminCheck = require('../middleware/adminCheck');
 
 reservationsRouter.post('/', reservationsController.createReservation);
 reservationsRouter.get('/:id', reservationsController.getReservation);
 reservationsRouter.get('/user/:userId', reservationsController.getReservationsByUser);
-reservationsRouter.put('/:id', reservationsController.updateReservation);
 reservationsRouter.delete('/:id', reservationsController.deleteReservation);
+reservationsRouter.get('/', adminCheck, reservationsController.getAllReservations);
+reservationsRouter.put('/:id', adminCheck, reservationsController.updateReservation);
 
 module.exports = reservationsRouter;
